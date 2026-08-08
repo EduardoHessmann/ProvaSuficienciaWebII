a62b0fce6865a6ebd5dc5f1f5014dc9cba33a4b3
Eduardo Hessmann <eduardo.h.rohleder@gmail.com>
2026-08-08 13:19:24 -0300

    Configuracao inicial do projeto com Swagger e conexao PostgreSQL

---
+++ ProvaSuficienciaWebII/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona os serviços ao container.
+
+builder.Services.AddControllers();
+
+// Configuração do Swagger (documentação e interface de testes da API).
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configura o pipeline de requisições HTTP.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
